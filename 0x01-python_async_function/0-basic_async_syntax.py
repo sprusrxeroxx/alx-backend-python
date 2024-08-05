0fb5d75cf429b73c4fd2839435edd6990767b3c6
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-08-05 15:41:20 +0200

    add Async tasks

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,10 +3,10 @@
 import random 
 import asyncio
 
-""" Write an asynchronous coroutine that takes in an integer argument that waits for a random time 
-    before it returns it
-"""
-
-async def wait_random(max_delay=10, type=float):
+async def wait_random(max_delay=10, type=int):
+    """
+        Write an asynchronous coroutine that takes in an integer argument that waits for a random time 
+        before it returns it
+    """
     await asyncio.sleep(random.randint(0, max_delay))
     return max_delay
