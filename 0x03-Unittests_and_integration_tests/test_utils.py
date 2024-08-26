61c0bc741b83a602ae3835ff4bf5fb2871c948c6
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-08-26 12:57:22 +0000

    test: implement test for memoization method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import Mock, patch
 from typing import (
     Mapping,
@@ -111,3 +111,40 @@ class TestGetJson(unittest.TestCase):
             response = get_json(test_url)
             self.assertEqual(response, test_payload)
             mock_response.json.assert_called_once()
+
+    class TestMemoize(unittest.TestCase):
+        """Test case class for memoize function 
+            
+            This method tests that when calling a_property twice,the correct result is returned 
+            but a_method is only called once using assert_called_once.
+        """
+
+        def test_memoize(self):
+            """
+                Test the 'memoize' decorator functionality.
+
+                This test method creates a test class with a memoized property and
+                verifies that the underlying method is called only once, even when
+                the property is accessed multiple times.
+
+                Returns:
+                    None: Asserts the behavior of the 'memoize' decorator.
+
+                Raises:
+                    AssertionError: If the expected behavior is not
+                    observed during testing.
+            """
+            class TestClass:
+
+                def a_method(self):
+                    return 42
+
+                @memoize
+                def a_property(self):
+                    return self.a_method()
+
+            with patch.object(TestClass, "a_method", return_value=42) as mocked:
+                spec = TestClass()
+                spec.a_property
+                spec.a_property
+                mocked.asset_called_once()
