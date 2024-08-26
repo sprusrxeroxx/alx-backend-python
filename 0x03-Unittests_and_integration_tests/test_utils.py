02cb11ab615b565140e0e5184151c41bf37affb9
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-08-26 10:42:08 +0000

    test: implemented test class for access_nested_map method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,1 +1,46 @@
 #!/usr/bin/env python3
+
+"""Test module for github org client utilities"""
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+from unittest.mock import Mock, patch
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+        Test that the 'access_nested_map' function returns the expected output
+        when given a map and a nested path.
+
+        Args:
+            map (dict): The input dictionary.
+            path (tuple): The nested path to access within the dictionary.
+            expected_output: The expected output when
+            accessing the specified path.
+
+        Returns:
+            None: This method asserts the equality of
+            the actual and expected outputs.
+
+        Raises:
+            AssertionError: If the actual output does not
+            match the expected output.
+    """
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
+
+    def test_access_nested_map(self, map, path, expected_output):
+        output = access_nested_map(map, path)
+        self.assertEqual(output, expected_output)    
